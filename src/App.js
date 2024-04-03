d705b7f218ca25cd1d6dc3319ec65214e78f1d9e
Artem Viatkin <newyorkbox47@gmail.com>
2024-04-03 16:35:45 +0300

    add auth-reducer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
-import Profile from "./components/Profile/Profile";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import News from "./components/News/News";
 import UsersContainer from "./components/Users/UsersContainer";
@@ -10,12 +8,13 @@ import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="app-wrapper">
-        <Header />
+        <HeaderContainer />
         <Navbar />
         <div className="app-wrapper-content">
           <Routes>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,16 @@
 import React from "react";
-import s from './Header.module.css';
+import s from "./Header.module.css";
+import { NavLink } from "react-router-dom";
 
-const Header = () =>{
-    return(
-        <header className={s.header}>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1PzLiCdOQ-9SUeGg3-BQbJF5npJKh7udkcg&usqp=CAU"></img>
-      </header>
-    )
-}
+const Header = (props) => {
+  return (
+    <header className={s.header}>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1PzLiCdOQ-9SUeGg3-BQbJF5npJKh7udkcg&usqp=CAU"></img>
+      <div className={s.loginBlock}>
+        {props.isAuth ? props.login : <NavLink to={"/login"}>Login</NavLink>}
+      </div>
+    </header>
+  );
+};
 
 export default Header;
