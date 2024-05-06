104ecbbb35d9c5e9d6dfc524f23bd497b411afcc
Artem Viatkin <newyorkbox47@gmail.com>
2024-05-06 15:05:11 +0300

    add redux devtools

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -6,6 +6,7 @@ import authReducer from "./auth-reducer";
 import { thunk } from "redux-thunk";
 import { reducer as formReducer } from "redux-form";
 import appReducer from "./app-reducer";
+import { compose } from "redux";
 
 let reducers = combineReducers({
   profilePage: profileReducer,
@@ -18,7 +19,13 @@ let reducers = combineReducers({
 
 const thunkMiddleware = thunk;
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(thunkMiddleware))
+);
+
+//let store = createStore(reducers, applyMiddleware(thunkMiddleware));
 
 window.store = store;
 
